8d7371241622172a5a8d2e17b1621cc5aa7f4456
jaegoog <svaxtica@gmail.com>
2021-07-28 13:56:46 +0900

    모델 추가

--- cnn_classifier_1.py
+++ cnn_classifier_1.py
@@ -5,3 +5,13 @@ dataset, info = tfds.load("imdb_reviews/subwords8k", with_info=True, as_supervis
 
 train_dataset, test_dataset, dataset["train"], dataset["test"]
 
+model = tf.keras.Sequencial(
+    [
+        tf.keras.layers.Embedding(10000, 64),
+        tf.keras.layers.Conv1D(128, 5, activation="relu"),
+        tf.keras.layers.GlobalAveragePolling1D(),
+        tf.keras.layers.Dense(64, activation="relu"),
+        tf.keras.layers.Dense(1, activation="sigmoid"),
+    ]
+)
+
